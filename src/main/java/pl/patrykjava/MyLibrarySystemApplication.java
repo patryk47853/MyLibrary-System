df70615513d882fe675ae7f2632df33e47bb97d6
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-14 17:19:22 +0100

    Create project

---
+++ src/main/java/pl/patrykjava/MyLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package pl.patrykjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibrarySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLibrarySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/patrykjava/MyLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patrykjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibrarySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
