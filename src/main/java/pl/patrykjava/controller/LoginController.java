285e638f22cc87ce5b879e8b2df3ee114c3ff2ba
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-17 16:34:35 +0100

    Create mapping for loginPage and registerUser

--- src/main/java/pl/patrykjava/controller/LoginController.java
+++ src/main/java/pl/patrykjava/controller/LoginController.java
@@ -1,13 +1,23 @@
 package pl.patrykjava.controller;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import pl.patrykjava.entity.User;
 
-@RestController
+
+@Controller
 public class LoginController {
 
     @GetMapping("/")
-    public String home() {
-        return "MyLibrary System - test home page";
+    public String loginPage() {
+        return "loginPage";
+    }
+
+    @GetMapping("/register")
+    public String registerUser(Model theModel) {
+
+        theModel.addAttribute("user", new User());
+
+        return "registerUser";
     }
 }
