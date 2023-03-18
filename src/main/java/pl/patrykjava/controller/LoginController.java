f9865115ee806b3a8aeb9df5b9f2c9289691e299
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-18 19:32:58 +0100

    Create mapping for processing registration (might me redefined)

--- src/main/java/pl/patrykjava/controller/LoginController.java
+++ src/main/java/pl/patrykjava/controller/LoginController.java
@@ -1,12 +1,21 @@
 package pl.patrykjava.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.patrykjava.entity.User;
+import pl.patrykjava.repository.UserRepository;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/")
     public String loginPage() {
@@ -19,5 +28,16 @@ public class LoginController {
         theModel.addAttribute("user", new User());
 
         return "registerUser";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistration(User user) {
+
+        user.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+        user.setActive(false);
+
+        userRepository.save(user);
+
+        return "registerSuccess";
     }
 }
