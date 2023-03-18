0136118cc6185a46f43c1d7b659a900b9a4b43e0
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-18 19:31:00 +0100

    Change User structure

--- src/main/java/pl/patrykjava/entity/User.java
+++ src/main/java/pl/patrykjava/entity/User.java
@@ -32,24 +32,6 @@ public class User {
     @Column(name = "active")
     private boolean active;
 
-    @Column(name = "first_name")
-    private String firstName;
-
-    @Column(name = "last_name")
-    private String lastName;
-
-    @Column(name = "phone_number")
-    private String phoneNumber;
-
-    @Column(name = "address")
-    private String address;
-
-    @Column(name = "postal_code")
-    private String postalCode;
-
-    @Column(name = "city")
-    private String city;
-
     @Column(name = "created_at")
     private Timestamp createdAt;
 
