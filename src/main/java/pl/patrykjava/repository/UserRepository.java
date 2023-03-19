77498f11e2899ad5c5191fb170102d05a5421508
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-19 21:11:55 +0100

    Define custom query and add findByEmail method

--- src/main/java/pl/patrykjava/repository/UserRepository.java
+++ src/main/java/pl/patrykjava/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package pl.patrykjava.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.patrykjava.entity.User;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @Query("SELECT u from User u where u.email = ?1")
+    User findByEmail(String email);
 
 }
