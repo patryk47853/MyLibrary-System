efcff612ba73d5e94b2695b4cf94e836ea3e98f7
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-21 17:22:49 +0100

    Add UserRegisterDTO

---
+++ src/main/java/pl/patrykjava/dto/UserRegisterDTO.java
@@ -0,0 +1,23 @@
+package pl.patrykjava.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegisterDTO {
+
+    private String username;
+    private String email;
+    private String password;
+
+    private Timestamp createdAt;
+
+    private boolean active;
+}
