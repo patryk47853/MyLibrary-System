fdce23203d1d128906b4ff6db68b8f6a9c4c5847
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-22 16:56:42 +0100

    Add Role entity

---
+++ src/main/java/pl/patrykjava/entity/Role.java
@@ -0,0 +1,26 @@
+package pl.patrykjava.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
