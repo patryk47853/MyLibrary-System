61c35d9c2be93587d17dbf65c59906306dcc4dd2
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-23 17:11:26 +0100

    Redefine database

--- database.sql
+++ database.sql
@@ -1,28 +1,43 @@
-CREATE USER 'patryk47853'@'localhost' IDENTIFIED BY 'patryk47853';
-
-GRANT ALL PRIVILEGES ON  *.* TO 'patryk47853'@'localhost';
-
 CREATE DATABASE  IF NOT EXISTS `mylibrary_system`;
 
 USE `mylibrary_system`;
 
+CREATE TABLE `roles` (
+                         `id` int(6) NOT NULL AUTO_INCREMENT,
+                         `name` varchar(45) NOT NULL UNIQUE,
 
-DROP TABLE IF EXISTS `users`;
+                         PRIMARY KEY (`id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `users` (
-	`id` int(6) NOT NULL AUTO_INCREMENT,
-    `username` varchar(64) NOT NULL,
-    `password` varchar(64) NOT NULL,
-    `email` varchar(64) NOT NULL,
-    `active` boolean DEFAULT FALSE,
-    `created_at` timestamp,
+                         `id` int(6) NOT NULL AUTO_INCREMENT,
+                         `username` varchar(100) NOT NULL UNIQUE,
+                         `email` varchar(255) NOT NULL UNIQUE,
+                         `password` varchar(255) NOT NULL,
+                         `enabled` boolean NOT NULL,
+                         `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
 
-	PRIMARY KEY (`id`)
-)
+                         PRIMARY KEY (`id`)
 
-CREATE TABLE `roles` (
-	`id` int(6) NOT NULL AUTO_INCREMENT,
-    `name` varchar(64) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	PRIMARY KEY (`id`)
-)
+CREATE TABLE `users_roles` (
+                               `users_id` int(6) NOT NULL,
+                               `roles_id` int(6) NOT NULL,
+
+                               PRIMARY KEY (`users_id`, `roles_id`),
+
+                               KEY `users` (`users_id`),
+                               CONSTRAINT `FK_USERS_AUTH` FOREIGN KEY (`users_id`)
+                                   REFERENCES `users` (`id`)
+                                   ON DELETE NO ACTION ON UPDATE NO ACTION,
+
+                               KEY `roles` (`roles_id`),
+                               CONSTRAINT `FK_USERS_ROLES` FOREIGN KEY (`roles_id`)
+                                   REFERENCES `roles` (`id`)
+                                   ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `roles` (`name`) VALUES
+                                 ("USER"), ("LIBRARIAN"), ("ADMIN");
