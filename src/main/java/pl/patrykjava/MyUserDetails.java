e2b1fbe1f3894c2ea1906b4763677dc869023955
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-24 17:26:53 +0100

    Create MyUserDetails

---
+++ src/main/java/pl/patrykjava/MyUserDetails.java
@@ -0,0 +1,62 @@
+package pl.patrykjava;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import pl.patrykjava.entity.Role;
+import pl.patrykjava.entity.User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class MyUserDetails implements UserDetails {
+
+    private User user;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<Role> roles = user.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
