66a419dddef05f99ed18f050b53ddb485cf3630c
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-26 19:57:13 +0200

    Add BookCategory entity (to be refined)

---
+++ src/main/java/pl/patrykjava/entity/BookCategory.java
@@ -0,0 +1,22 @@
+package pl.patrykjava.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "book_category")
+public class BookCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+}
