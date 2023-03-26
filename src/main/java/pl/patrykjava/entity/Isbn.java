9a3bdba05f158ff9bc8a82bf71e53cb600ddfba8
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-26 19:57:22 +0200

    Add Isbn entity (to be refined)

---
+++ src/main/java/pl/patrykjava/entity/Isbn.java
@@ -0,0 +1,19 @@
+package pl.patrykjava.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "isbn")
+public class Isbn {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+}
