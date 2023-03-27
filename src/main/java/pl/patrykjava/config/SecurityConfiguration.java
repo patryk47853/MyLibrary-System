dc4ce341b744c91f81412f01abdc6650b742207b
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-27 16:15:51 +0200

    Cosmetic changes

--- src/main/java/pl/patrykjava/config/SecurityConfiguration.java
+++ src/main/java/pl/patrykjava/config/SecurityConfiguration.java
@@ -44,7 +44,6 @@ public class SecurityConfiguration {
     String[] staticResources  =  {
             "/css/**",
             "/images/**",
-            "/fonts/**",
             "/scripts/**",
     };
 
