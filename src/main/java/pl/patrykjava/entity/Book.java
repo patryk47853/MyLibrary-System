48aac2a618c4495600a2df3bf4943be8ebc00b80
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-27 16:15:08 +0200

    Cosmetic changes (currently commented to make program runnable)

--- src/main/java/pl/patrykjava/entity/Book.java
+++ src/main/java/pl/patrykjava/entity/Book.java
@@ -1,52 +1,52 @@
-package pl.patrykjava.entity;
-
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.Set;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@Entity
-@Table(name = "book")
-public class Book {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private int id;
-
-    @Column(name = "google_id")
-    private String googleId;
-
-    @Column(name="title")
-    private String title;
-
-    @Column(name="page_count")
-    private int pageCount;
-
-    @Column(name="description")
-    private String description;
-
-    private Set<BookCategory> categories;
-
-    @Column(name="rating")
-    private double rating;
-
-    private Set<Author> authors;
-
-    @Column(name="publisher")
-    private String publisher;
-
-    @Column(name="published_at")
-    private String publishedAt;
-
-    private Isbn isbn;
-
-    @Column(name="image_link")
-    private String imageLink;
-}
+//package pl.patrykjava.entity;
+//
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import java.util.Set;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@Entity
+//@Table(name = "books")
+//public class Book {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column(name = "id")
+//    private int id;
+//
+//    @Column(name = "google_id")
+//    private String googleId;
+//
+//    @Column(name="title")
+//    private String title;
+//
+//    @Column(name="page_count")
+//    private int pageCount;
+//
+//    @Column(name="description")
+//    private String description;
+//
+//    private Set<BookCategory> categories;
+//
+//    @Column(name="rating")
+//    private double rating;
+//
+//    private Set<Author> authors;
+//
+//    @Column(name="publisher")
+//    private String publisher;
+//
+//    @Column(name="published_at")
+//    private String publishedAt;
+//
+//    private Isbn isbn;
+//
+//    @Column(name="image_link")
+//    private String imageLink;
+//}
