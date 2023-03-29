735a2d52ce51159d5b7d5394b7c2bb3194e6019e
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-29 16:18:39 +0200

    Create LibraryCardDTO

---
+++ src/main/java/pl/patrykjava/dto/LibraryCardDTO.java
@@ -0,0 +1,36 @@
+package pl.patrykjava.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LibraryCardDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private Date birthday;
+
+    private String address;
+
+    private String postalCode;
+
+    private String city;
+
+    private Timestamp createdAt;
+}
