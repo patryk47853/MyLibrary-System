ed1e19e82783f24c6fed37155a11913461942300
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-03-29 16:19:24 +0200

    Redefine HomeController

--- src/main/java/pl/patrykjava/controller/HomeController.java
+++ src/main/java/pl/patrykjava/controller/HomeController.java
@@ -2,15 +2,18 @@ package pl.patrykjava.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import pl.patrykjava.dto.LibraryCardDTO;
+import pl.patrykjava.service.LibraryCardService;
 import pl.patrykjava.service.UserService;
 
 @Controller
 public class HomeController {
-
     @Autowired
-    private UserService userService;
+    private LibraryCardService libraryCardService;
 
     @GetMapping("/books")
     public String books() {
@@ -18,15 +21,19 @@ public class HomeController {
         return "books";
     }
 
-    @GetMapping("/library-card")
-    public String libraryCard() {
+    @GetMapping("/create-library-card")
+    public String libraryCard(Model theModel) {
+
+        theModel.addAttribute("libraryCard", new LibraryCardDTO());
 
         return "createLibraryCard";
     }
 
-    @PostMapping("process_library_card")
-    public String createLibraryCard() {
+    @PostMapping("process-library-card")
+    public String createLibraryCard(@ModelAttribute("libraryCard") LibraryCardDTO libraryCardDTO) {
 
-        return "redirect:/library-card";
+        libraryCardService.save(libraryCardDTO);
+
+        return "redirect:/create-library-card";
     }
 }
