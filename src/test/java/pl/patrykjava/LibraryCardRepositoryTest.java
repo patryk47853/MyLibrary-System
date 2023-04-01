8655d80f1b831921be53f7b86e06ee66b2ec80fb
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-04-01 21:13:25 +0200

    Create SetLibraryCardToUser test

--- src/test/java/pl/patrykjava/LibraryCardRepositoryTest.java
+++ src/test/java/pl/patrykjava/LibraryCardRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.annotation.Rollback;
 import pl.patrykjava.entity.LibraryCard;
 import pl.patrykjava.entity.User;
 import pl.patrykjava.repository.LibraryCardRepository;
+import pl.patrykjava.repository.UserRepository;
 
 import java.util.List;
 
@@ -20,38 +21,61 @@ import java.util.List;
 @Rollback(value = false)
 class LibraryCardRepositoryTest {
 
-        @Autowired
-        private LibraryCardRepository libraryCardRepository;
+    @Autowired
+    private LibraryCardRepository libraryCardRepository;
 
-        @Test
-        @Order(1)
-        public void testCreateLibraryCard() {
-            LibraryCard libraryCard = new LibraryCard();
-            libraryCard.setFirstName("Patryk");
-            libraryCard.setLastName("Testowy");
-            libraryCard.setPhoneNumber("1412412");
-            libraryCard.setCity("Warsaw");
-            libraryCard.setPostalCode("01-341");
+    @Autowired
+    private UserRepository userRepository;
 
-            libraryCardRepository.save(libraryCard);
+    @Test
+    @Order(1)
+    public void testCreateLibraryCard() {
+        LibraryCard libraryCard = new LibraryCard();
+        libraryCard.setFirstName("Patryk");
+        libraryCard.setLastName("Testowy");
+        libraryCard.setPhoneNumber("1412412");
+        libraryCard.setCity("Warsaw");
+        libraryCard.setPostalCode("01-341");
 
-            List<LibraryCard> myCards = libraryCardRepository.findAll();
+        libraryCardRepository.save(libraryCard);
+
+        List<LibraryCard> myCards = libraryCardRepository.findAll();
 
 
-            Assertions.assertThat(myCards.size()).isEqualTo(1);
-        }
-
-        @Test
-        @Order(2)
-        public void testDeleteLibraryCards() {
-            List<LibraryCard> cards = libraryCardRepository.findAll();
-
-            for(LibraryCard card : cards) {
-                libraryCardRepository.delete(card);
-            }
-
-            List<LibraryCard> allCards = libraryCardRepository.findAll();
-
-            Assertions.assertThat(allCards.size()).isEqualTo(0);
-        }
+        Assertions.assertThat(myCards.size()).isEqualTo(1);
     }
+
+    @Test
+    @Order(2)
+    public void testDeleteLibraryCards() {
+        List<LibraryCard> cards = libraryCardRepository.findAll();
+
+        for (LibraryCard card : cards) {
+            libraryCardRepository.delete(card);
+        }
+
+        List<LibraryCard> allCards = libraryCardRepository.findAll();
+
+        Assertions.assertThat(allCards.size()).isEqualTo(0);
+    }
+
+    @Test
+    @Order(3)
+    public void testSetLibraryCardToUser() {
+        User user = new User("patryk2135", "to@dziala.com", "todziala");
+
+        // Create a UserProfile instance
+        LibraryCard libraryCard = new LibraryCard("Patryk", "Testowy", "Patryk",
+                "Testowy", "Patryk", "Testowy");
+
+
+        // Set child reference(userProfile) in parent entity(user)
+        user.setLibraryCard(libraryCard);
+
+        // Set parent reference(user) in child entity(userProfile)
+        libraryCard.setUser(user);
+
+        // Save Parent Reference (which will save the child as well)
+        userRepository.save(user);
+    }
+}
