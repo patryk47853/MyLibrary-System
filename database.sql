e6121251e020e3ee0dd8f425da1647a37311ceee
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-04-01 21:12:13 +0200

    Add Reader role and redefine database

--- database.sql
+++ database.sql
@@ -37,27 +37,26 @@ CREATE TABLE `users_roles` (
                                CONSTRAINT `FK_USERS_ROLES` FOREIGN KEY (`roles_id`)
                                    REFERENCES `roles` (`id`)
                                    ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `library_card`;
+DROP TABLE IF EXISTS `library_cards`;
 
-CREATE TABLE `library_card` (
+CREATE TABLE `library_cards` (
   `id` int(6) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(80) DEFAULT NULL,
   `last_name` varchar(80) DEFAULT NULL,
   `phone_number` varchar(80) DEFAULT NULL,
-  `birthday` date DEFAULT NULL,
   `address` varchar(120) DEFAULT NULL,
   `postal` varchar(80) DEFAULT NULL,
   `city` varchar(80) DEFAULT NULL,
   `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `user_id` int(6) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_USERS_CARDS` (`user_id`),
-  CONSTRAINT `FK_LIBRARY_CARD` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+  `user_id` INT UNIQUE,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    PRIMARY KEY (`id`)
+  
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
 INSERT INTO `roles` (`name`) VALUES
-                                 ("USER"), ("LIBRARIAN"), ("ADMIN");
+                                 ("USER"), ("READER"), ("LIBRARIAN"), ("ADMIN");
