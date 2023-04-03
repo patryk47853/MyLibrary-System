2f6b09b6ce2e406212801577c995f283e490f3fc
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-04-03 17:43:28 +0200

    Create mapping for user's profile (library card)

--- src/main/java/pl/patrykjava/controller/HomeController.java
+++ src/main/java/pl/patrykjava/controller/HomeController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.patrykjava.dto.LibraryCardDTO;
+import pl.patrykjava.entity.LibraryCard;
 import pl.patrykjava.entity.User;
+import pl.patrykjava.repository.LibraryCardRepository;
 import pl.patrykjava.repository.UserRepository;
 import pl.patrykjava.service.LibraryCardService;
 import pl.patrykjava.service.UserService;
@@ -21,6 +23,8 @@ public class HomeController {
     @Autowired
     private LibraryCardService libraryCardService;
 
+    @Autowired
+    private LibraryCardRepository libraryCardRepository;
     @Autowired
     private UserRepository userRepository;
 
@@ -39,11 +43,47 @@ public class HomeController {
     }
 
     @PostMapping("process-library-card")
-    public String createLibraryCard(@ModelAttribute("libraryCard") LibraryCardDTO libraryCardDTO) {
+    public String createLibraryCard(@ModelAttribute("libraryCard") LibraryCardDTO libraryCardDTO,
+                                    @AuthenticationPrincipal UserDetails currentUser) {
+
+        User user = userRepository.findByUsername(currentUser.getUsername());
+        if(user.getLibraryCard() != null) {
+            return "redirect:/create-library-card?error";
+        }
 
         libraryCardService.save(libraryCardDTO);
 
         return "redirect:/create-library-card?success";
+    }
+
+    @GetMapping("/profile")
+    public String showProfile(Model theModel,
+                              @AuthenticationPrincipal UserDetails currentUser) {
+
+        User user = userRepository.findByUsername(currentUser.getUsername());
+        LibraryCard libraryCard = user.getLibraryCard();
+
+        theModel.addAttribute("currentUser", libraryCard);
+
+        return "profile";
+    }
+
+    @GetMapping("/update-profile")
+    public String updateProfile(Model theModel,
+                              @AuthenticationPrincipal UserDetails currentUser) {
+
+        User user = userRepository.findByUsername(currentUser.getUsername());
+        LibraryCard libraryCard = user.getLibraryCard();
+
+        theModel.addAttribute("currentUser", libraryCard);
+
+        return "updateProfile";
+    }
+
+    @PostMapping("/process-update-profile")
+    public String processUpdateProfile() {
+
+        return "redirect:/update-profile?success";
     }
 
     @GetMapping("/users")
