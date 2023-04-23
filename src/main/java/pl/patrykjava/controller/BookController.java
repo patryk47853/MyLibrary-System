b0f33e678ca98242e321ebe540a336092e681ecf
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-04-23 15:09:40 +0200

    Add working book searching method, based on user's query via Google Books API (to be expanded)

--- src/main/java/pl/patrykjava/controller/BookController.java
+++ src/main/java/pl/patrykjava/controller/BookController.java
@@ -3,30 +3,64 @@ package pl.patrykjava.controller;
 import com.google.api.services.books.Books;
 import com.google.api.services.books.BooksRequestInitializer;
 import com.google.api.services.books.model.Volume;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.api.services.books.model.Volumes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import pl.patrykjava.entity.Book;
 
-@RestController
+@Controller
 public class BookController {
 
-    @GetMapping("/search")
-    public List<Volume> searchBooks(@RequestParam String query) {
+    @Value("${google.books.api.key}")
+    private String apiKey;
+    public List<Book> searchBooks(@RequestParam String query, Long startIndex) {
         try {
             Books books = new Books.Builder(new com.google.api.client.http.javanet.NetHttpTransport(), new com.google.api.client.json.jackson2.JacksonFactory(), null)
                     .setApplicationName("MyLibrarySearch")
-                    .setGoogleClientRequestInitializer(new BooksRequestInitializer("GOOGLE.API.KEY"))
+                    .setGoogleClientRequestInitializer(new BooksRequestInitializer(apiKey))
                     .build();
 
-            Volumes volumes = books.volumes().list(query).execute();
+            Volumes volumes = books.volumes().list(query)
+                    .setStartIndex(startIndex)
+                    .setMaxResults(6L)
+                    .execute();
 
-            return volumes.getItems();
+            List<Book> myBooks = new ArrayList<>();
+
+            for (Volume volume : volumes.getItems()) {
+                Book book = new Book();
+                book.setTitle(volume.getVolumeInfo().getTitle());
+                book.setAuthors(volume.getVolumeInfo().getAuthors());
+
+                myBooks.add(book);
+            }
+
+            return myBooks;
         } catch (Exception e) {
             System.err.println("Exception during API call: " + e);
             return null;
         }
     }
+
+    @GetMapping("/books")
+    public String showSearchResults(@RequestParam("query") String query, @RequestParam(value = "page", defaultValue = "0") int page, Model model) {
+        Long startIndex = page * 6L;
+        List<Book> books = searchBooks(query, startIndex);
+        int totalRecords = 60; // assuming there are 60 books for the given query
+
+        model.addAttribute("books", books);
+        model.addAttribute("query", query);
+        model.addAttribute("totalPages", (int) Math.ceil(totalRecords / 6.0));
+        model.addAttribute("currentPage", page);
+
+        return "books";
+    }
+
 }
