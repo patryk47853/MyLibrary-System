38b995a789fa63ce70be50f00b466a4c7361bd14
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-07-02 21:59:46 +0200

    Update LibrarianBookService (separate the business logic from the interface)

--- src/main/java/pl/patrykjava/controller/LibrarianController.java
+++ src/main/java/pl/patrykjava/controller/LibrarianController.java
@@ -2,7 +2,6 @@ package pl.patrykjava.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
---
+++ src/main/java/pl/patrykjava/service/LibrarianBookServiceImpl.java
@@ -0,0 +1,50 @@
+package pl.patrykjava.service;
+
+import com.google.api.services.books.Books;
+import com.google.api.services.books.BooksRequestInitializer;
+import com.google.api.services.books.model.Volume;
+import com.google.api.services.books.model.Volumes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.patrykjava.entity.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LibrarianBookServiceImpl implements LibrarianBookService {
+
+    @Value("${google.books.api.key}")
+    private String apiKey;
+
+    @Override
+    public List<Book> searchBooks(@RequestParam String query, Long startIndex) {
+        try {
+            Books books = new Books.Builder(new com.google.api.client.http.javanet.NetHttpTransport(), new com.google.api.client.json.jackson2.JacksonFactory(), null)
+                    .setApplicationName("MyLibrarySearch")
+                    .setGoogleClientRequestInitializer(new BooksRequestInitializer(apiKey))
+                    .build();
+
+            Volumes volumes = books.volumes().list(query)
+                    .setStartIndex(startIndex)
+                    .setMaxResults(6L)
+                    .execute();
+
+            List<Book> myBooks = new ArrayList<>();
+
+            for (Volume volume : volumes.getItems()) {
+                Book book = new Book();
+                book.setTitle(volume.getVolumeInfo().getTitle());
+                book.setAuthors(volume.getVolumeInfo().getAuthors());
+
+                myBooks.add(book);
+            }
+
+            return myBooks;
+        } catch (Exception e) {
+            System.err.println("Exception during API call: " + e);
+            return null;
+        }
+    }
+}
