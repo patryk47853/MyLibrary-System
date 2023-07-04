dcf0f8ac8da7d255146253420c3c06c591170b9a
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-07-04 09:53:36 +0200

    Update RoleRepository tests

--- src/test/java/pl/patrykjava/RoleRepositoryTest.java
+++ src/test/java/pl/patrykjava/RoleRepositoryTest.java
@@ -1,5 +1,6 @@
 package pl.patrykjava;
 
+import lombok.SneakyThrows;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -16,46 +17,60 @@ import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Rollback(value = false)
 public class RoleRepositoryTest {
 
     @Autowired
     private RoleRepository roleRepository;
 
+    @SneakyThrows
     @Test
     @Order(1)
-    public void createRolesTest() {
-        Role user = new Role("USER");
-        Role librarian = new Role("LIBRARIAN");
-        Role admin = new Role("ADMIN");
+    public void createNewRoles_whenSaved_thenGetOk() {
+        //given
+        Role testRole = new Role("TEST");
+        Role newRole = new Role("NEW");
 
-        roleRepository.saveAll(List.of(user, librarian, admin));
+        //when
+        roleRepository.saveAll(List.of(testRole, newRole));
 
+        //to ensure that the roles are saved and available for deletion in the subsequent test method
+        Thread.sleep(1000);
         List<Role> listRoles = roleRepository.findAll();
 
-        Assertions.assertThat(listRoles.size()).isEqualTo(3);
+        //then (there are [4] main roles: USER, READER, LIBRARIAN, ADMIN, so 4 + 2 = 6)
+        Assertions.assertThat(listRoles.size()).isEqualTo(6);
     }
 
     @Test
     @Order(2)
-    public void deleteRolesTest() {
-        List<Role> roles = roleRepository.findAll();
+    public void givenRolesNames_whenDeleted_thenGetOk() {
+        //given
+        Role testRole = roleRepository.findByName("TEST");
+        Role newRole = roleRepository.findByName("NEW");
 
-        for(Role role : roles) {
-            roleRepository.delete(role);
-        }
+        //assert that the roles are found
+        Assertions.assertThat(testRole).isNotNull();
+        Assertions.assertThat(newRole).isNotNull();
 
-        Assertions.assertThat(roles.size()).isEqualTo(0);
+        //when
+        roleRepository.delete(testRole);
+        roleRepository.delete(newRole);
+
+        //then
+        List<Role> existingRoles = roleRepository.findAll();
+        Assertions.assertThat(existingRoles.size()).isEqualTo(4);
     }
 
     @Test
     @Order(3)
-    public void findRoleTest() {
-
+    public void givenRoleIdOne_whenIsEqualToUser_thenGetOk() {
+        //given
         Role shouldBeUser = roleRepository.getReferenceById(1);
-
         Role isUser = roleRepository.findByName("USER");
 
+        //then
         Assertions.assertThat(isUser.getName()).isEqualTo(shouldBeUser.getName());
     }
 }
