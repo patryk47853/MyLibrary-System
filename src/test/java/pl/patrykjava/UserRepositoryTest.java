5795aced1b0e898f88b0e3f85cccfa30f33aa1c1
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-07-04 09:52:54 +0200

    Code clean-up

--- src/test/java/pl/patrykjava/UserRepositoryTest.java
+++ src/test/java/pl/patrykjava/UserRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
 import pl.patrykjava.entity.Role;
 import pl.patrykjava.entity.User;
