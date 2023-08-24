fce11b322ae97ec806fb8db6932a11c9b1bb1da7
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-08-24 11:06:41 +0200

    Code clean-up

--- src/main/java/pl/patrykjava/service/LibrarianBookServiceImpl.java
+++ src/main/java/pl/patrykjava/service/LibrarianBookServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import pl.patrykjava.entity.Book;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -36,17 +37,7 @@ public class LibrarianBookServiceImpl implements LibrarianBookService {
             List<Book> myBooks = new ArrayList<>();
 
             for (Volume volume : volumes.getItems()) {
-                Book book = new Book();
-                book.setTitle(volume.getVolumeInfo().getTitle());
-                if (volume.getVolumeInfo().getAuthors() != null) {
-                    book.setAuthors(volume.getVolumeInfo().getAuthors());
-                }
-                book.setPublishedDate(volume.getVolumeInfo().getPublishedDate());
-                Integer pageCount = volume.getVolumeInfo().getPageCount();
-                String pageCountValue = pageCount != null && pageCount != 0 ? pageCount.toString() : "no data";
-                book.setPageCount(pageCountValue);
-                book.setCategories(volume.getVolumeInfo().getCategories());
-
+                Book book = createBookFromVolume(volume);
                 myBooks.add(book);
             }
 
@@ -55,5 +46,25 @@ public class LibrarianBookServiceImpl implements LibrarianBookService {
             System.err.println("Exception during API call: " + e);
             return null;
         }
+    }
+
+    private Book createBookFromVolume(Volume volume) {
+        Book book = new Book();
+
+        book.setTitle(volume.getVolumeInfo().getTitle());
+
+        List<String> authors = volume.getVolumeInfo().getAuthors();
+        book.setAuthors(authors != null ? authors : Collections.singletonList("no data"));
+
+        book.setPublishedDate(volume.getVolumeInfo().getPublishedDate());
+
+        Integer pageCount = volume.getVolumeInfo().getPageCount();
+        String pageCountValue = pageCount != null && pageCount != 0 ? pageCount.toString() : "no data";
+        book.setPageCount(pageCountValue);
+
+        List<String> categories = volume.getVolumeInfo().getCategories();
+        book.setCategories(categories != null ? categories : Collections.singletonList("no data"));
+
+        return book;
     }
 }
