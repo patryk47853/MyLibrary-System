2eedf418a35c3a9767eadeecb021654ff86af884
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-08-25 11:45:37 +0200

    Add showReaders mapping + small update to showSearchResults

--- src/main/java/pl/patrykjava/controller/LibrarianController.java
+++ src/main/java/pl/patrykjava/controller/LibrarianController.java
@@ -1,11 +1,17 @@
 package pl.patrykjava.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.patrykjava.entity.Book;
+import pl.patrykjava.entity.LibraryCard;
+import pl.patrykjava.repository.LibraryCardRepository;
 import pl.patrykjava.service.LibrarianBookService;
 
 @Controller
@@ -13,9 +19,11 @@ import pl.patrykjava.service.LibrarianBookService;
 public class LibrarianController {
 
     private final LibrarianBookService librarianBookService;
+    private final LibraryCardRepository libraryCardRepository;
 
-    public LibrarianController(LibrarianBookService librarianBookService) {
+    public LibrarianController(LibrarianBookService librarianBookService, LibraryCardRepository libraryCardRepository) {
         this.librarianBookService = librarianBookService;
+        this.libraryCardRepository = libraryCardRepository;
     }
 
     @GetMapping("/dashboard")
@@ -24,11 +32,16 @@ public class LibrarianController {
     }
 
     @GetMapping("/books")
-    public String showSearchResults(@RequestParam("query") String query, @RequestParam(value = "page", defaultValue = "0") int page, Model model) {
+    public String showSearchResults(@RequestParam(value = "query", required = false) String query, @RequestParam(value = "page", defaultValue = "0") int page, Model model) {
         Long startIndex = page * 8L;
         int booksPerPage = 8;
-        List<Book> books = librarianBookService.searchBooks(query, startIndex);
-        int totalRecords = 60; // assuming there are 60 books for the given query
+        List<Book> books = new ArrayList<>();
+        int totalRecords = 0;
+
+        if (query != null) {
+            books = librarianBookService.searchBooks(query, startIndex);
+            totalRecords = 60;
+        }
 
         model.addAttribute("books", books);
         model.addAttribute("query", query);
@@ -36,5 +49,16 @@ public class LibrarianController {
         model.addAttribute("currentPage", page);
 
         return "librarian/librarianSearchBooks";
+    }
+
+    @GetMapping("/readers")
+    public String showReaders(@PageableDefault(size = 5) Pageable pageable, Model theModel) {
+
+        Page<LibraryCard> libraryCards = libraryCardRepository.findAll(pageable);
+
+        theModel.addAttribute("readers", libraryCards.getContent());
+        theModel.addAttribute("page", libraryCards);
+
+        return "librarian/librarianReaders";
     }
 }
