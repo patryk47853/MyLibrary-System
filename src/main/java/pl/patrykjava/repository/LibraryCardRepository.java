861072047322615076f84f7827b909f665a56b67
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-08-29 10:24:58 +0200

    Add Page<LibraryCard> findByPhoneNumber method

--- src/main/java/pl/patrykjava/repository/LibraryCardRepository.java
+++ src/main/java/pl/patrykjava/repository/LibraryCardRepository.java
@@ -1,15 +1,18 @@
 package pl.patrykjava.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.patrykjava.entity.LibraryCard;
-
 
 @Repository
 public interface LibraryCardRepository extends JpaRepository<LibraryCard, Integer> {
 
     @Query("SELECT c from LibraryCard c where c.phoneNumber = ?1")
     LibraryCard findByPhoneNumber(String phoneNumber);
+
+    Page<LibraryCard> findByPhoneNumber(String phoneNumber, Pageable pageable);
 
 }
