a47efbe345ad376dcbd1d533a51c3c96185a9f91
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-09-03 15:21:50 +0200

    Create new tables: Authors, Categories and Books

--- database.sql
+++ database.sql
@@ -39,6 +39,29 @@ CREATE TABLE `users_roles` (
                                    ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
+CREATE TABLE authors (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(255)
+);
+
+CREATE TABLE categories (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            name VARCHAR(255)
+);
+
+CREATE TABLE books (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       title VARCHAR(255),
+                       publishedDate VARCHAR(50),
+                       pageCount VARCHAR(20),
+                       selfLink VARCHAR(255),
+                       description TEXT,
+                       author_id INT,
+                       category_id INT,
+                       FOREIGN KEY (author_id) REFERENCES authors(id),
+                       FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
 CREATE TABLE `library_cards` (
   `id` int(6) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(80) DEFAULT NULL,
