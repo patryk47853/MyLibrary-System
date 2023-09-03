6d6735c75647c170141888ed3b54c8108bf95153
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-09-03 15:22:23 +0200

    Update Book entity to match SQL table

--- src/main/java/pl/patrykjava/entity/Book.java
+++ src/main/java/pl/patrykjava/entity/Book.java
@@ -1,6 +1,5 @@
 package pl.patrykjava.entity;
 
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,20 +7,32 @@ import lombok.Setter;
 
 import java.util.List;
 
-
-
 @Getter
 @Setter
 @NoArgsConstructor
 @Table(name = "books")
 public class Book {
 
+    @Column(name = "title")
     private String title;
+
+    @ElementCollection
+    @CollectionTable(name = "authors", joinColumns = @JoinColumn(name = "book_id"))
     private List<String> authors;
+
+    @Column(name = "publishedDate")
     private String publishedDate;
 
+    @Column(name = "pageCount")
     private String pageCount;
+
+    @ElementCollection
+    @CollectionTable(name = "categories", joinColumns = @JoinColumn(name = "book_id"))
     private List<String> categories;
 
+    @Column(name = "selfLink")
     private String selfLink;
+
+    @Column(name = "description")
+    private String description;
 }
