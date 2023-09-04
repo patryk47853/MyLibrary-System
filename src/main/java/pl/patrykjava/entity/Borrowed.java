0a6a04f8db4dc5e2d5d6205f830c18b1c46a28c4
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-09-04 10:34:59 +0200

    Out-comment Borrowed class (because now the project isn't working properly with this class)

--- src/main/java/pl/patrykjava/entity/Borrowed.java
+++ src/main/java/pl/patrykjava/entity/Borrowed.java
@@ -1,41 +1,41 @@
-package pl.patrykjava.entity;
-
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.util.List;
-
-import jakarta.persistence.*;
-
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
-@NoArgsConstructor
-@Entity
-@Table(name="borrowed")
-public class Borrowed {
-
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id")
-    private int id;
-
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    private List<Borrowed> borrowedBooks;
-
-    @ManyToOne(fetch=FetchType.LAZY)
-    @JoinColumn(name="users_id")
-    private User user;
-
-    @Column(name="dated")
-    private Timestamp dated;
-
-    @Column(name="due_date")
-    private Date dueDate;
-}
+//package pl.patrykjava.entity;
+//
+//import java.sql.Date;
+//import java.sql.Timestamp;
+//import java.util.List;
+//
+//import jakarta.persistence.*;
+//
+//import lombok.EqualsAndHashCode;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Getter
+//@Setter
+//@EqualsAndHashCode
+//@ToString
+//@NoArgsConstructor
+//@Entity
+//@Table(name="borrowed")
+//public class Borrowed {
+//
+//    @Id
+//    @GeneratedValue(strategy=GenerationType.IDENTITY)
+//    @Column(name="id")
+//    private int id;
+//
+//    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+//    private List<Borrowed> borrowedBooks;
+//
+//    @ManyToOne(fetch=FetchType.LAZY)
+//    @JoinColumn(name="users_id")
+//    private User user;
+//
+//    @Column(name="dated")
+//    private Timestamp dated;
+//
+//    @Column(name="due_date")
+//    private Date dueDate;
+//}
