a93ddac45142dcefd14a29f03ede2b35ff458548
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-09-13 16:45:35 +0200

    Create Isbn entity

---
+++ src/main/java/pl/patrykjava/entity/Isbn.java
@@ -0,0 +1,25 @@
+package pl.patrykjava.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "isbn")
+public class Isbn {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="isbn_10", unique=true)
+    private String isbn10;
+
+    @Column(name="isbn_13", unique=true)
+    private String isbn13;
+}
