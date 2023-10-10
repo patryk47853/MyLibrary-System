a5673115ce5ac3610f6c3d2dfa3b8ec6cb15025b
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-10-10 12:01:15 +0200

    Update SQL script (fix not registering user bug)

--- database.sql
+++ database.sql
@@ -1,70 +1,52 @@
-CREATE DATABASE IF NOT EXISTS `mylibrary_system`;
+CREATE DATABASE mylibrary_system;
+USE mylibrary_system;
 
-USE `mylibrary_system`;
-
-CREATE TABLE `roles` (
-                         `id` int(6) NOT NULL AUTO_INCREMENT,
-                         `name` varchar(45) NOT NULL UNIQUE,
-
-                         PRIMARY KEY (`id`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `users` (
-                         `id` int(6) NOT NULL AUTO_INCREMENT,
-                         `username` varchar(100) NOT NULL UNIQUE,
-                         `email` varchar(255) NOT NULL UNIQUE,
-                         `password` varchar(255) NOT NULL,
-                         `enabled` boolean NOT NULL,
-                         `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-
-                         PRIMARY KEY (`id`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `users_roles` (
-                               `users_id` int(6) NOT NULL,
-                               `roles_id` int(6) NOT NULL,
-
-                               PRIMARY KEY (`users_id`, `roles_id`),
-
-                               KEY `users` (`users_id`),
-                               CONSTRAINT `FK_USERS_AUTH` FOREIGN KEY (`users_id`)
-                                   REFERENCES `users` (`id`)
-                                   ON DELETE NO ACTION ON UPDATE NO ACTION,
-
-                               KEY `roles` (`roles_id`),
-                               CONSTRAINT `FK_USERS_ROLES` FOREIGN KEY (`roles_id`)
-                                   REFERENCES `roles` (`id`)
-                                   ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE roles (
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       name VARCHAR(45) NOT NULL UNIQUE
 );
 
-CREATE TABLE `library_cards` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(80) DEFAULT NULL,
-  `last_name` varchar(80) DEFAULT NULL,
-  `phone_number` varchar(80) DEFAULT NULL,
-  `address` varchar(120) DEFAULT NULL,
-  `postal` varchar(80) DEFAULT NULL,
-  `city` varchar(80) DEFAULT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `user_id` INT UNIQUE,
-    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
-    PRIMARY KEY (`id`)
-  
-  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+CREATE TABLE users (
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       username VARCHAR(100) NOT NULL UNIQUE,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL,
+                       enabled BOOLEAN NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE TABLE `books` (
-                        `id` int(6) NOT NULL AUTO_INCREMENT,
-                        `title` varchar(255) NOT NULL,
-                        `page_count` int(6) NOT NULL,
-                        `rating` decimal(3, 2) NOT NULL,
-                        `description` text NOT NULL,
-                        `image_link` varchar(255) NOT NULL,
-                        PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE library_cards (
+                               id INT PRIMARY KEY AUTO_INCREMENT,
+                               first_name VARCHAR(80),
+                               last_name VARCHAR(80),
+                               phone_number VARCHAR(80),
+                               address VARCHAR(120),
+                               postal VARCHAR(80),
+                               city VARCHAR(80),
+                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                               user_id INT UNIQUE,
+                               FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
-SET FOREIGN_KEY_CHECKS = 1;
+CREATE TABLE users_roles (
+                             user_id INT,
+                             role_id INT,
+                             PRIMARY KEY (user_id, role_id),
+                             FOREIGN KEY (user_id) REFERENCES users(id),
+                             FOREIGN KEY (role_id) REFERENCES roles(id)
+);
 
-INSERT INTO `roles` (`name`) VALUES
-                                 ("USER"), ("READER"), ("LIBRARIAN"), ("ADMIN");
+CREATE TABLE books (
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       title VARCHAR(255) NOT NULL,
+                       cover_image_url VARCHAR(255),
+                       publisher VARCHAR(255),
+                       publishedDate VARCHAR(255),
+                       pageCount INT,
+                       google_books_id VARCHAR(255) NOT NULL,
+                       average_rating DECIMAL(3, 2),
+                       selfLink VARCHAR(255),
+                       description TEXT
+);
+
+INSERT INTO roles (name) VALUES ('USER'), ('READER'), ('LIBRARIAN'), ('ADMIN');
