0b0043a1b8b97bbb08fb216f93282dd0ccfba63b
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-10-22 16:26:08 +0200

    Uncomment Author and Category entities

--- src/main/java/pl/patrykjava/entity/Author.java
+++ src/main/java/pl/patrykjava/entity/Author.java
@@ -1,22 +1,22 @@
-//package pl.patrykjava.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "authors")
-//public class Author {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    @Column(name = "id")
-//    private int id;
-//
-//    @Column(name = "name", unique = true)
-//    private String name;
-//}
+package pl.patrykjava.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+}
--- src/main/java/pl/patrykjava/entity/Category.java
+++ src/main/java/pl/patrykjava/entity/Category.java
@@ -1,22 +1,24 @@
-//package pl.patrykjava.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "categories")
-//public class Category {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    @Column(name = "id")
-//    private int id;
-//
-//    @Column(name = "name", unique = true)
-//    private String name;
-//}
+package pl.patrykjava.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+}
