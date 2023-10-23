b1aeaafc49dce31d94d15edac7d9cb97247b2fdd
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-10-23 18:16:20 +0200

    Add findByName method both to CategoryRepository and AuthorRepository

--- src/main/java/pl/patrykjava/repository/AuthorRepository.java
+++ src/main/java/pl/patrykjava/repository/AuthorRepository.java
@@ -1,10 +1,14 @@
 package pl.patrykjava.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.patrykjava.entity.Author;
 
 @Repository
 public interface AuthorRepository extends JpaRepository <Author, Integer> {
+
+    @Query("SELECT a from Author a where a.name = ?1")
+    Author findByName(String name);
 
 }
--- src/main/java/pl/patrykjava/repository/CategoryRepository.java
+++ src/main/java/pl/patrykjava/repository/CategoryRepository.java
@@ -1,9 +1,14 @@
 package pl.patrykjava.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.patrykjava.entity.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    @Query("SELECT c from Category c where c.name = ?1")
+    Category findByName(String name);
+
 }
