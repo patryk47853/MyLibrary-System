03c10a467b1254f98938fd9915e10ee945e5e711
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-10-25 11:19:15 +0200

    Add findBookByGoogleBooksId method

--- src/main/java/pl/patrykjava/repository/BookRepository.java
+++ src/main/java/pl/patrykjava/repository/BookRepository.java
@@ -1,10 +1,14 @@
 package pl.patrykjava.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.patrykjava.entity.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    @Query("SELECT b from Book b where b.googleBooksId = ?1")
+    Book findBookByGoogleBooksId(String googleBooksId);
 
 }
