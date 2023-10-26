e9502f978d2011d5b7d02386f2a30959918adeb5
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-10-26 11:19:07 +0200

    Add findBookByTitleContaining and findBooksByTitleContaining methods

--- src/main/java/pl/patrykjava/repository/BookRepository.java
+++ src/main/java/pl/patrykjava/repository/BookRepository.java
@@ -5,10 +5,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.patrykjava.entity.Book;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
     @Query("SELECT b from Book b where b.googleBooksId = ?1")
     Book findBookByGoogleBooksId(String googleBooksId);
+
+    @Query("SELECT b FROM Book b WHERE b.title LIKE %?1%")
+    Book findBookByTitleContaining(String title);
+
+    @Query("SELECT DISTINCT b FROM Book b JOIN FETCH b.authors WHERE b.title LIKE %?1%")
+    List<Book> findBooksByTitleContaining(String title);
 
 }
